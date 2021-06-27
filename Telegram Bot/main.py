bb41558b15caabb1d467812e75f68b09bb7d70f4
jagadish-ravichandran <65297790+jagadish-ravichandran@users.noreply.github.com>
2021-06-27 12:13:14 +0530

    fixed

--- Telegram Bot/main.py
+++ Telegram Bot/main.py
@@ -3,11 +3,15 @@ from bs4 import BeautifulSoup
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 
 # Bot Name : Insta Downloader
-# Developed by : Jagadish
+# Author : Jagadish
 
 header = {
             "User-Agent" : "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:89.0) Gecko/20100101 Firefox/89.0"
     }
+
+## Replace this api_token with yours
+api_token = API_TOKEN
+
 
 def start_command(update, context):
     context.bot.send_message(chat_id = update.message.chat_id,text = f"Hello {update.effective_user.first_name}! \nI'm here to download instagram image apart from private accounts")
@@ -17,7 +21,7 @@ def start_command(update, context):
 def imageDownload(update, context):
     url = update.message.text
 
-    if 'https://www.instagram.com/' not in url:
+    if not url.startswith('https://www.instagram.com/'):
         context.bot.send_message(chat_id = update.message.chat_id, text = "It may not be instagram link !\nCheck once again and send me")
         return
 
@@ -51,8 +55,7 @@ def no_text_handling(update, context):
 
 def main():
     
-    ## Replace this api_token with yours
-    api_token = API_TOKEN
+    
     updater = Updater(api_token, use_context = True)
     dispatcher = updater.dispatcher
     dispatcher.add_handler(CommandHandler("start",start_command))
